c2f01b64a56583d550a17e268df9076eabf410dc
Aanye <spthom+aanye@gmail.com>
2013-03-06 21:06:00 -0700

    Initial commit

---
+++ Aanye_Mail/Locales/enUS.lua
@@ -0,0 +1,11 @@
+
+local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Aanye_Mail", "enUS", true)
+if not L then return end
+
+L["Aanye_Mail"] = true
+
+L["New Mail"] = true
+L["No Mail"] = true
+
+L["Unread mail from:"] = true
+L["No unread mail."] = true
